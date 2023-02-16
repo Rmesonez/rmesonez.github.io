eb09ef9badfda1474e6c890c60a9794b873480e3
Rmesonez <rmesonez@gmail.com>
2023-02-15 18:34:05 -0600

    update 19

---
+++ carrito-RM/assets/js/database.js
@@ -0,0 +1,36 @@
+const sliderDb = [
+    {
+      id: 12,
+      title:"AlienWare Notebook 18",
+      src:"https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/alienware_m18.jpg",
+      price: 1000.99,
+      category: "Laptops",
+      quantity: 7,
+    },
+    {
+      id: 13,
+      title:"Alienware Extern Case ",
+      src:"https://assets-prd.ignimgs.com/2023/01/19/alienwareroundup-1674153708969.jpg",
+      price: 1599.99,
+      category: "Computers",
+      quantity: 5,
+    },
+    {
+      id: 14,
+      title:"AlienWare Laptop",
+      src:"https://i.dell.com/das/xa.ashx/global-site-design%20WEB/5e899e9f-c003-db6b-403f-1fa03e97ad29/1/OriginalJPG?id=Dell/Product_Images/Dell_Client_Products/Notebooks/Alienware_Notebooks/Alienware_15/Non_touch/global_spi/laptop-alienware-15-non-touch-hero-504x350-ng.psd",
+      price: 1299.99,
+      category: "Laptops",
+      quantity: 3,
+    },
+    {
+      id: 15,
+      title:"AlienWare NoteBook 16",
+      src:"https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/alienware_x16_6.jpg",
+      price: 899.99,
+      category: "Laptops",
+      quantity: 7,
+    } 
+  ]
+
+  
